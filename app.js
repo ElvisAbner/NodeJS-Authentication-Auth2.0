487c60fc187e788acabd83b24d1566cda3b1ab2a
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-08-27 18:58:36 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-//jshint esversion:6
+const express = require('express')
+const ejs = require('ejs')
+const exp = require('constants')
+
+const app = express()
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, () => {
+    console.log('Server Running on Port 3000')
+})
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
